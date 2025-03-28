8f27766599eb1467c4f9c790d04cb8d093714a4f
Amitesh Rai <amiteshrai@outlook.com>
2025-03-29 03:07:40 +0530

    Added conversational history using LanChain memories ]

--- src/apps/main.py
+++ src/apps/main.py
@@ -1,30 +1,57 @@
-# src/main.py
-"""Main module for the project."""
-# from openai import OpenAI
-from langchain_openai.llms import OpenAI
+"""Main Module"""
+
+from langchain.prompts import HumanMessagePromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory
+from langchain_community.chat_message_histories import FileChatMessageHistory
+
+
 from configs.env_configs import OPENAI_API_KEY
 
 
-def print_msg(greeting: str) -> None:
-    """_summary_
+# This is a simple example of how to use LangChain with OpenAI's ChatGPT API
+# The ChatPromptTemplate is used to create a chat prompt
+# The HumanMessagePromptTemplate is used to create a human message prompt
+# The MessagesPlaceholder is used to create a placeholder for messages
+prompt = ChatPromptTemplate(
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
+    input_variables=["content", "messages"],
+)
 
-    Args:
-        greeting (str): _description_
-    """
-    print(greeting)
+# The ChatOpenAI is used to create a chat model
+llm = ChatOpenAI(
+    api_key=OPENAI_API_KEY,
+    verbose=True
+)
 
+# The ConversationBufferMemory is used to create a memory buffer for the conversation
+memory = ConversationSummaryMemory(
+    chat_memory=FileChatMessageHistory("chat_history.json"),
+    memory_key="messages",
+    return_messages=True,
+    llm=llm,
+    max_token_limit=1000
+)
 
-def main() -> None:
-    """_summary_
-    """
-    print_msg("Hello, World!")
-    print_msg("Hola, Mundo!")
-    print_msg("Goodbye, World!")
+# The LLMChain is used to create a chain of operations
+chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    memory=memory,
+    verbose=True
+)
 
-
-if __name__ == "__main__":
-    main()
-    llm = OpenAI(api_key=OPENAI_API_KEY)
-
-    result = llm.invoke("Write a very short poen about a cat.")
-    print(result)
+# Here, there is now= mechanism to store the conversation history
+while True:
+    content = input(">> ")
+    if content == "exit":
+        break
+    response = chain({"content": content})
+    # print(response)
+    print(response["text"])
